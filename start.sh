b5b5c5a774bb2ddf7f0360b67392f8004006a300
root <root@hem>
2026-04-29 18:49:38 +0300

    Fix Railway deployment settings and Gunicorn start script

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ PORT=${PORT:-8000}
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-exec gunicorn pos.wsgi:application --bind 0.0.0.0:$PORT
+# exec gunicorn pos.wsgi:application --bind 0.0.0.0:$PORT
+exec gunicorn pos.wsgi:application --bind 0.0.0.0:${PORT:-8080}
